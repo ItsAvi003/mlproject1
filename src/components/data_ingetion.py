96ce0368d152bc78ba212437974bddf5336f2e8e
ItsAvi003 <iandiehard@gmail.com>
2025-08-28 21:42:36 +0530

    data ingetio

---
+++ src/components/data_ingetion.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path=os.path.join('artifacts',"train_data.csv")
+    test_data_path=os.path.join('artifacts',"test_data.csv")
+    raw_data_path=os.path.join('artifacts',"raw_data.csv")
+
+class IngetionData:
+    def __init__(self):
+        self.ingetion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingetion methord or components")
+
+        try:
+            df=pd.read_csv("notebook\data\stud.csv")
+            logging.info("Exported the dataset as DataFrame")
+
+            os.makedirs(os.path.dirname(self.ingetion_config.train_data_path),exist_ok=True)
+            
+            df.to_csv(self.ingetion_config.raw_data_path,index=False,header=True)
+            logging.info("Train Test Split ocuuring")
+
+            train_set,test_set=train_test_split(df,test_size=0.20,random_state=42)
+
+            train_set.to_csv(self.ingetion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingetion_config.test_data_path,index=False,header=True)
+            logging.info("Ingetion completed")
+
+            return(
+                self.ingetion_config.train_data_path,
+                self.ingetion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=IngetionData()
+    obj.initiate_data_ingestion()
+
+
+
